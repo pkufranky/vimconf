0a09b0c19ccd1f6072ec4d8e41dc6956edc6dacd
host8 <host8>
2001-10-13 22:31:42 +0000

    Added file skelet.c.

---
+++ vim/skelet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Function int main(int argc, char **argv) {{{
+int main(int argc, char **argv)
+{
+
+
+	return 0;
+} // }}}
+
+
+/* Modeline for ViM {{{1
+ * vim:set ts=4:
+ * vim600:fdm=marker fdl=0 fdc=3:
+ * }}}1 */
+
