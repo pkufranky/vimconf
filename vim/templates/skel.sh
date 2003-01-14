b680ffee37faa7d168987f35cfaeba54c310da80
rajo <rajo>
2003-01-14 14:25:32 +0000

    Nicer templates/skeletons.

--- vim/templates/skel.sh
+++ vim/templates/skel.sh
@@ -3,6 +3,18 @@
 
 #!/bin/sh
 
+#
+# @FILE_EXT@
+#
+# Developed by @AUTHOR@ <@EMAIL@>
+# Copyright (c) @YEAR@ @COMPANY@
+# Licensed under terms of GNU General Public License.
+# All rights reserved.
+#
+# Changelog:
+# @DATE@ - created
+#
+
 # $Id$
 # $Platon$
 
